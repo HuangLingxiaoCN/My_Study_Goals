5a9de1619ac3a8ba882e234f74ba330fea58de93
Lingxiao Huang <jasonhlx1213@gmail.com>
2021-07-25 05:43:58 +0300

    create localStorage to store goal list, fetch goals and remove goal

--- script.js
+++ script.js
@@ -1,43 +1,73 @@
+const myGoalForm = document.getElementById("myForm");
 const input = document.getElementById("input");
-const button = document.getElementById("button");
 const goal_list = document.getElementById("goal_list");
-const deleteBtns = document.querySelectorAll('.deleteBtn');
 
-let shopping_item;
+let myGoals = [];
 
-const inputHandler = e => {
-  shopping_item = e.target.value;
-};
-
-const sumbitHandler = event => {
-  // Shopping list Item
-  const newItem = document.createElement('li');
-  newItem.textContent = " " + shopping_item;
+// Create a new DOM list
+const createNewGoal = (goal) => {
+  // Create Goal list Item
+  const newItem = document.createElement("li");
+  newItem.textContent = " " + goal;
   // Add New Icon
-  const newIcon = document.createElement('i');
-  newIcon.classList.add('fas', 'fa-bullseye');
+  const newIcon = document.createElement("i");
+  newIcon.classList.add("fas", "fa-bullseye");
   // Delete button
-  const deleteButton = document.createElement('button');
-  deleteButton.classList.add('deleteBtn');
-  deleteButton.textContent = 'Delete';
+  const deleteButton = document.createElement("button");
+  deleteButton.classList.add("deleteBtn");
+  deleteButton.textContent = "Delete";
   deleteButton.onclick = (e) => {
     let deleteNode = e.target;
     deleteNode.parentNode.remove(deleteNode);
+    removeGoal(goal);
   };
-  // Append to the shopping list
+  // Append to the Goal list
   newItem.prepend(newIcon);
   newItem.append(deleteButton);
   goal_list.appendChild(newItem);
-  // Reset the input to be empty
-  input.value = '';
 };
 
-input.addEventListener("change", inputHandler);
-button.addEventListener("click", sumbitHandler);
-// Add event listeners for the hard coded buttons
-deleteBtns.forEach(btn => {
-  btn.addEventListener('click', (e) => {
-    let deleteNode = e.target;
-    deleteNode.parentNode.remove(deleteNode);
-  })
-});
+// Populate myGoals to the DOM tree
+const populateGoals = () => {
+  // If there is no goals in localStorage, add myGoals to localStorage and populate them
+  if (!localStorage.getItem("myGoals") || localStorage.getItem('myGoals') === '[]') {
+    myGoals = [
+      "JavaScript: noob to veteran",
+      "React: zero to mastery",
+      "Node: beginning to finish",
+      "Typescript essentials",
+    ];
+    localStorage.setItem("myGoals", JSON.stringify(myGoals));
+    myGoals.forEach((goal) => {
+      createNewGoal(goal);
+    });
+  } else {
+    myGoals = JSON.parse(localStorage.getItem("myGoals"));
+    myGoals.forEach((goal) => {
+      createNewGoal(goal);
+    });
+  }
+};
+
+const addGoal = (e) => {
+  e.preventDefault();
+  // Pull out the input value
+  const newGoal = input.value;
+  myGoals.push(newGoal);
+  localStorage.setItem('myGoals', JSON.stringify(myGoals))
+  createNewGoal(newGoal);
+  input.value = "";
+};
+
+const removeGoal = (deletedGoal) => {
+  myGoals.forEach((goal, i) => {
+    if(goal == deletedGoal) {
+      myGoals.splice(i, 1);
+      localStorage.setItem('myGoals', JSON.stringify(myGoals));
+    }
+  });
+};
+
+myGoalForm.addEventListener("submit", addGoal);
+
+populateGoals();
